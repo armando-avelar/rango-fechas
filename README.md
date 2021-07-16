999a6e0043949c732b9a00533dc37006f7cc6e5a
Armando Avelar <armando.avelar@outlook.com>
2021-07-16 10:25:26 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Validación de dos rangos de fechas
+
+- instalar dependencias
+
+    ```
+    npm install
+    ```
---
+++ index.js
@@ -0,0 +1,31 @@
+const moment = require('moment');
+
+(() => {
+
+    let rango1 = {
+        inicio : moment().startOf('day'),
+        fin: moment().add(5, 'days').startOf('day')
+    }
+
+    let rango2 = {
+        inicio : moment().add(-5, 'days').startOf('day'),
+        fin: moment().add(-2, 'days').startOf('day')
+    }
+
+    if(rango1.inicio.isBefore(rango2.inicio)){
+        if(rango2.inicio.isBefore(rango1.fin)){
+            console.log('estas en el rango 1');
+            return
+        }
+    }
+
+    if(rango2.inicio.isBefore(rango1.inicio)){
+        if(rango2.fin.isAfter(rango1.inicio)){
+            console.log('estas en el rango 1');
+            return;
+        }
+    }
+
+    console.log('fechas validas');
+    
+})();
